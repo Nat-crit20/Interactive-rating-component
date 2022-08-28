0af88d53b80ddfe9a25b0805a3a35d9f3caebed4
Nathan Greuel <greuelnathan74@gmail.com>
2022-08-28 17:12:22 -0500

    Finished project

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const button = document.querySelectorAll("button");
 const form = document.querySelector("form");
+const input = document.querySelector("input");
+const formCard = document.querySelector(".form-card");
+const thanksCard = document.querySelector(".thanks-card");
+const thanksRating = document.querySelector("#thanks-rating");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(e.value);
+  const data = new FormData(form);
+  let output;
+  for (const value of data.values()) {
+    output = `<p>You selected ${value} out of 5</p>`;
+  }
+  thanksRating.innerHTML = output;
+  formCard.style.display = "none";
+  thanksCard.style.display = "contents";
 });
